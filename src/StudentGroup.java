48da49e76ebdb6bbf07d25f03aaead045f68f663
Pruthvinath67 <pruthvinath67@gmail.com>
2017-09-12 15:24:31 +0530

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -91,7 +91,7 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void remove(int index) {
 		// Add your implementation here
-		if(index<0||index>=students.length) {
+		/*if(index<0||index>=students.length) {
 			throw new IllegalArgumentException();
 		}
 		else {
@@ -99,7 +99,7 @@ public class StudentGroup implements StudentArrayOperation {
 			for(int i=index;i<students.length;i++) {
 				student[i]=student[i+1];
 			}
-		}
+		}*/
 	}
 
 	@Override
@@ -171,6 +171,21 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public Student getNextStudent(Student student) {
 		// Add your implementation here
-		return null;
+		try {
+			int flag=0;
+			for(int i=0;i<students.length;i++) {
+				flag=students[i].compareTo(student);
+				if(flag==1)
+					break;
+			}
+			if(flag==1) {
+				return students[flag+1];
+			}
+			else
+				return null;
+		}
+		catch(IllegalArgumentException e) {
+		System.out.println(e);
+		}
 	}
 }
